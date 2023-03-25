15865d5607c0ab8c3519a17bfa03b12fc0b1d4df
hrishikesh <hshinde166@gmail.com>
2023-03-26 01:41:57 +0530

    authorization files

---
+++ server/controllers/auth.js
@@ -0,0 +1,53 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const register = async (req, res) => {
+  try {
+    const {
+      firstname,
+      lastname,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstname,
+      lastname,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000), //random values for now
+      impressions: Math.floor(Math.random() * 10000),
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//Logging in
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ msg: "User does not exist" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ msg: "Wrong password" });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  delete user.password; //deleting the fetched password so that we dont send it to the frontend in the form of response
+  res.status(200).json(token, user);
+};
