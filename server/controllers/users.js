cc3c999de8a09fabde73c3ebe6fe49229fd97a8a
hrishikesh <hshinde166@gmail.com>
2023-03-30 00:40:16 +0530

    Add remove friend function

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -38,5 +38,40 @@ export const getUserFriends = async (req, res) => {
 //Update
 export const addRemoveFriend = async (req, res) => {
   try {
-  } catch (err) {}
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== id);
+    } else {
+      user.friends.push(friendId);
+      friend.friends.push(id);
+    }
+
+    const friends = await Promise.all(
+      user.friends.map((id) => {
+        User.findById(id);
+      })
+    );
+
+    //formatting the friends object and sending only the necessary information as reponse
+    const formattedFriends = friends.map(
+      ({ _id, firstname, lastname, occupation, picturePath, location }) => {
+        return {
+          _id,
+          firstname,
+          lastname,
+          occupation,
+          picturePath,
+          location,
+        };
+      }
+    );
+
+    res.status(200).json(formattedFriends);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
 };
