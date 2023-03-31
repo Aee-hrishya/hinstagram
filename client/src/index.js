f0fe6fa44110f468284845f142d8e43d69b91946
hrishikesh <hshinde166@gmail.com>
2023-04-01 02:04:35 +0530

    minor change

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import authReducer from "./states";
 import { Provider } from "react-redux";
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import {
   persistStore,
   persistReducer,
